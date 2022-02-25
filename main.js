6010cd4124abd6f727b4280813785d7ecfb53cca
Nae Cosmin <cosminnae00@yahoo.com>
2022-02-25 22:51:14 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+let addNoteBtn = document.querySelector('.submit');
+let container = document.querySelector('.note-app');
+
+addNoteBtn.addEventListener('click', addNote);
+
+function addNote(e) {
+  let textInput = document.querySelector('.text').value;
+
+  let div = document.createElement('div');
+  div.className = 'note-bubble';
+  container.appendChild(div);
+
+  let title = document.createElement('h2');
+  title.className = 'title number';
+  div.appendChild(title);
+
+  let span = document.createElement('span');
+  span.className = 'span-name';
+  span.appendChild(document.createTextNode(0));
+  title.appendChild(span);
+  span.style.display = 'none';
+
+  let value = document.querySelector('.span-name').innerHTML;
+  value++;
+  document.querySelector('.span-name').innerHTML = value;
+
+  title.appendChild(document.createTextNode(`Note ${value}`));
+
+  let text = document.createElement('p');
+  text.className = 'text-bubble';
+  text.appendChild(document.createTextNode(textInput));
+  div.appendChild(text);
+
+  let button = document.createElement('button');
+  button.className = 'view-detail';
+  button.appendChild(document.createTextNode('View More'));
+  div.appendChild(button);
+
+  button.addEventListener('click', viewMore);
+
+  //   function viewMore(e) {
+
+  //   }
+}
